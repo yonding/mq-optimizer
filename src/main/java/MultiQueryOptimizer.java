00e123a66b39382e10233f996d0ee17b9f748c09
yonding <uwrgoy7584@khu.ac.kr>
2023-12-06 00:29:50 +0900

    Feat: 같은 조인 조건을 가진 질의들끼리 하나의 조인으로 묶어주는 메서드

--- src/main/java/MultiQueryOptimizer.java
+++ src/main/java/MultiQueryOptimizer.java
@@ -1,2 +1,44 @@
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.tools.RelBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiQueryOptimizer {
+    public static List<List<Query>> separateQuery(List<Query> queryList) {
+        List<List<Query>> batchList = new ArrayList<>();
+        int[] queryFlagArray = new int[queryList.size()];
+        int index = 0;
+        for (int i = 0; i < queryList.size() - 1; i++) {
+            if (queryFlagArray[i] == 1) continue;
+            for (int j = i + 1; j < queryList.size(); j++) {
+                if (queryFlagArray[j] == 0 && queryList.get(i).joinEquals(queryList.get(j))) {
+                    if (queryFlagArray[i] == 0) { // if it is the first equal case,
+                        batchList.add(new ArrayList<Query>());
+                        batchList.get(index).add(queryList.get(i));
+                        queryFlagArray[i] = 1;
+                    }
+                    batchList.get(index).add(queryList.get(j));
+                    queryFlagArray[j] = 1;
+                }
+            }
+            if (queryFlagArray[i] == 0) {
+                batchList.add(new ArrayList<Query>());
+                batchList.get(index).add(queryList.get(i));
+            }
+            index++;
+        }
+        return batchList;
+    }
+
+    public static RelNode makeGlobalQuery(RelBuilder relBuilder, List<Query> queryList) {
+        RelNode relNode;
+
+        relNode = relBuilder.scan("user").build();
+
+        return relNode;
+    }
+
+
 }
